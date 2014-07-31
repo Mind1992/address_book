8673f8d22ea6e346d9dcfca1620c5fae9e79b120
Sergii Mertsalov <sergmerts@gmail.com>
2014-07-30 21:51:38 -0700

    update specs with newer syntax

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -4,15 +4,15 @@ require 'contact'
 describe Contact do
   it 'lets you read name, phone number, email, mailing address' do
     test_contact = Contact.new('Mark', '(888) 9086-171', 'mark1971@gmail.com', '1250 SW 43th Ave')
-    test_contact.name.should eq 'Mark'
-    test_contact.phone.should eq '8889086171'
-    test_contact.email.should eq 'mark1971@gmail.com'
-    test_contact.address.should eq '1250 SW 43th Ave'
+    expect(test_contact.name).to eq 'Mark'
+    expect(test_contact.phone).to eq '(888) 9086-171'
+    expect(test_contact.email).to eq 'mark1971@gmail.com'
+    expect(test_contact.address).to eq '1250 SW 43th Ave'
   end
 
   it 'gets added to when add is called on an instance' do
-    test_contact = Contact.new('Mark', '8889086171', 'mark1971@gmail.com', '1250 SW 43th Ave')
+    test_contact = Contact.new('Mark', '(888) 9086-171', 'mark1971@gmail.com', '1250 SW 43th Ave')
     test_contact.add
-    Contact.all.should eq [test_contact]
+    expect(Contact.all).to eq [test_contact]
   end
 end
