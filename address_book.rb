e8d191ed4d15240a7708391c76f49428164234c5
Sergii Mertsalov <sergmerts@gmail.com>
2014-07-30 22:58:12 -0700

    add new contact

--- address_book.rb
+++ address_book.rb
@@ -2,5 +2,27 @@ require './lib/contact'
 
 def main_menu
 	loop do 
-		puts "Press 'a' to add a new contact	`	"
+		puts "***Address Book***"
+		puts "1: add contact"
+		puts "9: exit"
+		user_input = gets.chomp
+		if user_input == "1"
+			add_contact
+		elsif user_input == "9"
+			exit
+		else 
+			puts "This is the wrong command"
+		end
+	end
 end
+
+def add_contact
+ print "Add name: "; name = gets.chomp
+ print "Add phone number: "; phone = gets.chomp
+ print "Add email: "; email = gets.chomp
+ print "Add mailing address: "; address = gets.chomp
+ new_contact = Contact.new(name,phone,email,address)
+ new_contact.add
+ puts "New contact for #{new_contact.name} added"
+end
+main_menu
