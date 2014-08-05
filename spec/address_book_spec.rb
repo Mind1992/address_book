3ba4e182c64de99545c0329e82b6eae884dc0c6e
Sergii Mertsalov <sergmerts@gmail.com>
2014-08-04 21:59:23 -0700

    fix specs to pass with new classes

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -10,11 +10,11 @@ describe Contact do
   end
 
   it 'lets you read name, phone number, email, mailing address' do
-    test_contact = Contact.new('Mark', '(888) 9086-171', 'mark1971@gmail.com', '1250 SW 43th Ave')
+    test_contact = Contact.new('Mark')
     expect(test_contact.name).to eq 'Mark'
-    expect(test_contact.phone).to eq '(888) 9086-171'
-    expect(test_contact.email).to eq 'mark1971@gmail.com'
-    expect(test_contact.address).to eq '1250 SW 43th Ave'
+    expect(test_contact.phones).to eq []
+    expect(test_contact.emails).to eq []
+    expect(test_contact.addresses).to eq []
   end
 
 describe '.all' do
@@ -24,14 +24,14 @@ describe '.all' do
 end
 
   it 'gets added to when add is called on an instance' do
-    test_contact = Contact.new('Mark', '(888) 9086-171', 'mark1971@gmail.com', '1250 SW 43th Ave')
+    test_contact = Contact.new('Mark')
     test_contact.add
     expect(Contact.all).to eq [test_contact]
   end
 
   describe '.clear' do
-    it 'returns an empty dictionary' do
-      Contact.new('Mark', '(888) 9086-171', 'mark1971@gmail.com', '1250 SW 43th Ave').add
+    it 'returns an empty array' do
+      Contact.new('Mark').add
       Contact.clear
       expect(Contact.all).to eq []
     end
