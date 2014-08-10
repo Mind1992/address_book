5ffb0e539c5f528dc2625c19e7ebb1efb6b66340
Sergii Mertsalov <sergmerts@gmail.com>
2014-08-10 11:04:06 -0700

    update phone numbers

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -44,6 +44,14 @@ describe Phone do
     test_phone = Phone.new('(888) 9086-171')
     expect(test_phone.phone).to eq '(888) 9086-171'
   end
+
+  it 'lets you update phone numbers' do 
+    test_contact = Contact.new('Mark')
+    test_phone = Phone.new('(888) 9086-171')
+    test_contact.add_phone(test_phone)
+    test_contact.add
+    expect(test_contact.update('(888) 9086-171', '(503) 5482-321')).to eq '(503) 5482-321'
+  end
 end
 
 describe Email do
